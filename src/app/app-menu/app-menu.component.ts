be83041444041eabce1d9c6d236ca286b40a699c
denismaster <denismaster@outlook.com>
2018-04-12 01:18:14 +0300

    Make logo in AppMenu hideable

--- src/app/app-menu/app-menu.component.ts
+++ src/app/app-menu/app-menu.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './app-menu.component.html',
   styleUrls: ['./app-menu.component.css']
 })
-export class AppMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AppMenuComponent {
+  showLogo: boolean = false;
+  constructor(private element: ElementRef) {
   }
 
+  @HostListener('window:scroll', [])
+  public onWindowScroll(): void {
+    const pageYOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (this.showLogo && pageYOffset < this.element.nativeElement.offsetTop) {
+      this.showLogo = false;
+    } else if (pageYOffset > this.element.nativeElement.offsetTop) {
+      this.showLogo = true;
+    }
+  }
 }
+
