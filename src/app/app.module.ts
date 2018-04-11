335339e2625130e43f098862bf9cc9ec4dfde9ab
denismaster <denismaster@outlook.com>
2018-04-11 22:17:15 +0300

    Add header, menu and footer components skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMenuComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule
