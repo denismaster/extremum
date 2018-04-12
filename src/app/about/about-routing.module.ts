153172eddc8b4b8a4b11d756937687ce65c07d3b
denismaster <denismaster@outlook.com>
2018-04-12 08:44:41 +0300

    Add Requisites page

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RequisitesComponent } from './requisites/requisites.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "about/requisites",
+  component: RequisitesComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
+import { RequisitesComponent } from './requisites/requisites.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AboutRoutingModule
   ],
-  declarations: []
+  declarations: [RequisitesComponent]
 })
 export class AboutModule { }
