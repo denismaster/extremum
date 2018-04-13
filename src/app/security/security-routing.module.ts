d8dfa1b7bcf009929ce48eaed78c4337f095fcd7
denismaster <denismaster@outlook.com>
2018-04-13 08:40:05 +0300

    Add security page skeleton

--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SecurityComponent } from './security.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'security',
+  component: SecurityComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SecurityRoutingModule } from './security-routing.module';
+import { SecurityComponent } from './security.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SecurityRoutingModule
   ],
-  declarations: []
+  declarations: [SecurityComponent]
 })
 export class SecurityModule { }
