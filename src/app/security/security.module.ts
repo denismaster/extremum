d324119d716c9fa7b3cbb1b6f72a1f68f04e6388
denismaster <denismaster@outlook.com>
2018-04-20 02:39:58 +0300

    Modify contacts, add info section of security page

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SecurityRoutingModule } from './security-routing.module';
 import { SecurityComponent } from './security.component';
+import { SecurityInfoComponent } from './components/security-info/security-info.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SecurityRoutingModule
   ],
-  declarations: [SecurityComponent]
+  declarations: [SecurityComponent, SecurityInfoComponent]
 })
 export class SecurityModule { }
