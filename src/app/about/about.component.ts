3703bb045c537b6d46dd99338577161172ae7eaa
denismaster <denismaster@outlook.com>
2018-04-20 08:21:24 +0300

    Add ability to scroll to element through the page nav

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router:Router) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          const element = document.querySelector("#" + tree.fragment);
+          if (element) { element.scrollIntoView(); }
+        }
+      }
+    });
   }
-
 }
